afa02efd1eec8215587f25115e85451fa06ed9c9
AbidSiam3 <siamabed3@gmail.com>
2019-12-19 16:42:53 +0600

    final commit

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ const mongoose = require('mongoose');
 
 const graphqlSchema = require('./graphql/schema/index');
 const graphqlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 app.use(bodyParser.json());
 // app.get('/', (req,res ,next)=> {
 //     res.send("Hello!!");
 // });
-
+app.use(isAuth);
 app.use('/graphql', graphHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
@@ -20,7 +33,7 @@ app.use('/graphql', graphHttp({
 const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@multisiam-pugea.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 mongoose.connect(URI, {useNewUrlParser:  true, useUnifiedTopology: true})
 .then(()=> {
-    app.listen(4200, console.log("server running 4200"));
+    app.listen(8000, console.log("server running 8000"));
 }).catch(err => {
     console.log(err);
 })
